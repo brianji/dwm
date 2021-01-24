d983217faa14d6c26dec5c0fb2221710f80a652c
Brian Isganitis <brianji03@gmail.com>
2021-01-24 17:46:41 -0500

    Add placeholder for rules.

--- config.def.h
+++ config.def.h
@@ -28,6 +28,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
+        { NULL,       NULL,       NULL,       0,            False,       -1 },
 };
 
 /* layout(s) */
