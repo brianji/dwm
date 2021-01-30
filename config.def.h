c418e0040c9e767ace4307c13ba5b4f19be5ca19
Brian Isganitis <brianji03@gmail.com>
2021-01-30 18:42:32 -0500

    Add material icon rounded font.

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@ static const unsigned int gappx     = 32;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrains Mono:size=10" };
+static const char *fonts[]          = { "JetBrains Mono:size=10", "Material Icons Outlined:pixelsize=22" };
 static const char dmenufont[]       = "JetBrains Mono:size=10";
 static const char col_gray1[]       = "#2e3440";
 static const char col_gray2[]       = "#3b4252";
