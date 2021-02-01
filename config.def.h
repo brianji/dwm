45d4aed42b2c764f41dede7ddabf8a2db464d43f
Brian Isganitis <brianji03@gmail.com>
2021-01-31 23:35:16 -0500

    Add rule for discord.

--- config.def.h
+++ config.def.h
@@ -44,7 +44,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-        { NULL,       NULL,       NULL,       0,            False,       -1 },
+	{ "discord",  NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
